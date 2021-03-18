c27d20ed1039cf58e04fcbc393f0a3d92ce38977
Yaritza <yaritzamicaelleb@gmail.com>
2021-03-17 22:34:12 -0300

    Tarefa heron

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Heron
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Lado1, Lado2, Lado3;
+            double n1, n2, n3, semiperimetro, area;
+
+            Console.Beep();
+
+            Console.WriteLine("---------");
+            Console.WriteLine("Digite abaixo as medidas do triangulo, que serão calculadas");
+            Console.Write("Lado1: ");
+            Lado1 = Console.ReadLine();
+            Console.Write("Lado2: ");
+            Lado2 = Console.ReadLine();
+            Console.Write("Lado3: ");
+            Lado3 = Console.ReadLine();
+
+            n1= Convert.ToDouble(Lado1);
+            n2= Convert.ToDouble(Lado2);
+            n3= Convert.ToDouble(Lado3);
+
+            semiperimetro= (n1+n2+n3)/2;
+            area= Math.Sqrt (semiperimetro*(semiperimetro-n1)*(semiperimetro-n2)*(semiperimetro-n3));
+
+            Console.Beep();
+
+            Console.WriteLine("----------");
+            Console.ForegroundColor=ConsoleColor.DarkRed;
+            Console.WriteLine("\n---Area e Semiperimetro---");
+            Console .ResetColor ();
+            Console.WriteLine($"A área é: {area}");
+            Console.WriteLine($"O semiperimetro é: {semiperimetro}"); 
+            Console.WriteLine("----------");
+        }
+    }
+}
